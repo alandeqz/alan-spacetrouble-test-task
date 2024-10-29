3ff44fde796bb1d2d760e1d389f523aaae1af93c
alandeqz <alansexenali@gmail.com>
2024-10-29 19:25:26 +0100

    Modify the migrations

--- migrations/20241029181459_create_booking_schema.sql
+++ migrations/20241029181459_create_booking_schema.sql
@@ -6,30 +6,33 @@ CREATE TABLE IF NOT EXISTS "alan-tabeo-test-task"."bookings"
     id             BIGSERIAL PRIMARY KEY,
     created_at     TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    first_name     VARCHAR(255) NOT NULL,
-    last_name      VARCHAR(255) NOT NULL,
-    gender         SMALLINT NOT NULL,
-    birthday       DATE NOT NULL,
-    launchpad_id   VARCHAR(255) NOT NULL,
-    destination_id VARCHAR(255) NOT NULL,
+    first_name     VARCHAR(255)                NOT NULL,
+    last_name      VARCHAR(255)                NOT NULL,
+    gender         SMALLINT                    NOT NULL,
+    birthday       DATE                        NOT NULL,
+    launchpad_id   VARCHAR(255)                NOT NULL,
+    destination_id VARCHAR(255)                NOT NULL,
     launch_date    TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 
+COMMENT ON COLUMN "alan-tabeo-test-task"."bookings".gender IS 'Unknown=0;Male=1;Female=2;Other=3';
+
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
-RETURN NEW;
+    RETURN NEW;
 END;
 $$ language 'plpgsql';
 
 CREATE TRIGGER update_booking_updated_at
-    BEFORE UPDATE ON "alan-tabeo-test-task"."bookings"
+    BEFORE UPDATE
+    ON "alan-tabeo-test-task"."bookings"
     FOR EACH ROW
-    EXECUTE FUNCTION update_updated_at_column();
+EXECUTE FUNCTION update_updated_at_column();
 
 -- +goose StatementEnd
-
 
 
 -- +goose Down
